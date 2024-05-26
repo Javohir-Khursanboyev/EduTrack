6c1217e78508c53ade8321bdae60006ec09ce030
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-27 00:02:02 +0500

    Added AssingmentService

---
+++ src/EduTrack.Service/Services/Assignments/IAssignmentService.cs
@@ -0,0 +1,14 @@
+﻿using EduTrack.Service.Configurations;
+using EduTrack.Service.DTOs.Assignments;
+using X.PagedList;
+
+namespace EduTrack.Service.Services.Assignments;
+
+public interface IAssignmentService
+{
+    ValueTask<AssignmentViewModel> CreateAsync(AssignmentCreateModel createModel);
+    ValueTask<AssignmentViewModel> UpdateAsync(long id, AssignmentUpdateModel updateModel);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<AssignmentViewModel> GetByIdAsync(long id);
+    ValueTask<IPagedList<AssignmentViewModel>> GetAllAsync(int? page, Filter filter, string search = null);
+}
