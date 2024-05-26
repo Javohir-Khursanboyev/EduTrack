24a35aa51bf5a968120dcfa803bd37337456d24d
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-26 16:48:24 +0500

    Added UnitOfWork

--- src/EduTrack.Data/DbContexts/AppDbContext.cs
+++ src/EduTrack.Data/DbContexts/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EduTrack.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduTrack.Data.DbContexts;
 
@@ -6,4 +7,13 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
     {   }
+
+    public DbSet<Asset> Assets { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Lesson> Lessons { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Assignment> Assignments { get; set; }
+    public DbSet<Attendence> Attendences { get; set; }
+    public DbSet<AssignmentMark> AssignmentMarks { get; set; }
 }
---
+++ src/EduTrack.Data/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Arcana.DataAccess.Repositories;
+using EduTrack.Domain.Entities;
+
+namespace Arcana.DataAccess.UnitOfWorks;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<Student> Students { get; }
+    IRepository<Asset> Assets { get; }
+    IRepository<Teacher> Teachers { get; }
+    IRepository<Lesson> Lessons { get; }
+    IRepository<Group> Groups { get; }
+    IRepository<Attendence> Attendences { get; }
+    IRepository<Assignment> Assignments { get; }
+    IRepository<AssignmentMark> AssignmentMarks { get; }
+    ValueTask<bool> SaveAsync();
+    ValueTask BeginTransactionAsync();
+    ValueTask CommitTransactionAsync();
+}
