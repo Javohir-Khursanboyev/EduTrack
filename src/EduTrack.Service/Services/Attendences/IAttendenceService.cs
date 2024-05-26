bcc27e98cd05ed4a526b802ae5673d617242721d
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-26 23:56:45 +0500

    Added LessonService

---
+++ src/EduTrack.Service/Services/Attendences/IAttendenceService.cs
@@ -0,0 +1,14 @@
+﻿using EduTrack.Service.Configurations;
+using EduTrack.Service.DTOs.Attendences;
+using X.PagedList;
+
+namespace EduTrack.Service.Services.Attendences;
+
+public interface IAttendenceService
+{
+    ValueTask<AttendenceViewModel> CreateAsync(AttendenceCreateModel createModel);
+    ValueTask<AttendenceViewModel> UpdateAsync(long id, AttendenceUpdateModel updateModel);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<AttendenceViewModel> GetByIdAsync(long id);
+    ValueTask<IPagedList<AttendenceViewModel>> GetAllAsync(int? page, Filter filter, string search = null);
+}
