c672fa5878353319d0754377e5577d09b5e132c3
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-26 16:33:14 +0500

    Added AppDbContext

---
+++ src/EduTrack.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EduTrack.Data.DbContexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+    {   }
+}
