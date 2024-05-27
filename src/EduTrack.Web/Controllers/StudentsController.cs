305e2139d5944215017c7cd523666e9f35cdc5fb
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-27 10:56:08 +0500

    Added StudentListModel Model

---
+++ src/EduTrack.Web/Controllers/StudentsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduTrack.Web.Controllers;
+
+public class StudentsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
